16bc4c43d2cc13e8bf253b7276734784a648d2b7
pxhanus <pxhanus@gmail.com>
2015-12-04 13:00:29 -0600

    Initial commit - need to debug

---
+++ EvenAndOddNumbers/Form1.Designer.vb
@@ -0,0 +1,61 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnGenerate = New System.Windows.Forms.Button()
+        Me.lstDisplay = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'btnGenerate
+        '
+        Me.btnGenerate.Location = New System.Drawing.Point(66, 12)
+        Me.btnGenerate.Name = "btnGenerate"
+        Me.btnGenerate.Size = New System.Drawing.Size(75, 23)
+        Me.btnGenerate.TabIndex = 0
+        Me.btnGenerate.Text = "Generate"
+        Me.btnGenerate.UseVisualStyleBackColor = True
+        '
+        'lstDisplay
+        '
+        Me.lstDisplay.FormattingEnabled = True
+        Me.lstDisplay.Location = New System.Drawing.Point(12, 41)
+        Me.lstDisplay.Name = "lstDisplay"
+        Me.lstDisplay.Size = New System.Drawing.Size(183, 173)
+        Me.lstDisplay.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(207, 231)
+        Me.Controls.Add(Me.lstDisplay)
+        Me.Controls.Add(Me.btnGenerate)
+        Me.Name = "Form1"
+        Me.Text = "Even And Odd Numbers"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnGenerate As System.Windows.Forms.Button
+    Friend WithEvents lstDisplay As System.Windows.Forms.ListBox
+
+End Class
---
+++ EvenAndOddNumbers/Form1.vb
@@ -0,0 +1,57 @@
+﻿Public Class Form1
+
+    Private Sub btnGenerate_Click(sender As Object, e As EventArgs) Handles btnGenerate.Click
+        Dim evenNums As Integer
+        Dim oddNums As Integer
+        Dim returnEvenOdd As String = Nothing
+        Dim numbers(9) As Integer
+        Me.lstDisplay.Items.Clear()
+
+        For index As Integer = 0 To 9
+            numbers(index) = RndInt(1, 99)
+        Next
+
+        Call EvenOddNums(numbers, evenNums, oddNums, returnEvenOdd)
+        Call DisplayArray(Me.lstDisplay, numbers, returnEvenOdd)
+
+    End Sub
+    '********************************************************************************************
+    'RndInt
+    'Returns a random integer between max and min
+    '
+    'post:  A number is returned
+    '********************************************************************************************
+    Function RndInt(ByVal min As Integer, ByVal max As Integer) As Integer
+        Randomize()
+        Return Int((max - min + 1) * Rnd() + min)
+    End Function
+    '********************************************************************************************
+    'DisplayArray
+    'Displays the contents of the passed array in the passed list box values index
+    '
+    'post:  Array results displayed in list box 
+    '********************************************************************************************
+    Sub DisplayArray(ByRef lstbox As ListBox, ByRef array() As Integer, ByVal EvenOdd As String)
+        For index As Integer = 0 To array.Length - 1
+            lstbox.Items.Add(index & vbTab & array(index) & vbCrLf & EvenOdd)
+        Next
+    End Sub
+    '********************************************************************************************
+    'EvenOddNums
+    'sorts even and odd numbers
+    '
+    'post: returns even and odd values in a string
+    '********************************************************************************************
+    Sub EvenOddNums(ByRef array() As Integer, ByRef even As Integer, ByRef odd As Integer, _
+                    ByRef returnValue As String)
+        For i As Integer = 0 To array.Length - 1
+            If array(i) Mod 2 = 0 Then
+                even &= array(i) & vbCrLf
+            Else
+                odd &= array(i) & vbCrLf    'Error at i = 7
+            End If
+        Next
+        returnValue = "Even numbers in the array:" & vbCrLf & even & _
+            "Odd numbers in the array:" & vbCrLf & odd
+    End Sub
+End Class
